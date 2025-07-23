01a00a668bd16f6daa2505cd1317b29b9f7ba721
jegraphy <jegraphy@gmail.com>
2025-07-23 17:13:56 +0300

    Vercel build fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,6 @@ const nextConfig = {
   assetPrefix: process.env.NEXT_PUBLIC_BASE_URL || '',
   basePath: '',
   
-  // Vercel için static dosya optimizasyonu
-  experimental: {
-    optimizeCss: true,
-  },
-  
   // Static dosyalar için headers
   async headers() {
     return [
